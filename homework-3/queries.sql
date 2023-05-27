401e84773a70c0c91a7c5261b56e88df837f2845
Vagner Elena <dipnoe@gmail.com>
2023-05-28 00:25:03 +0300

    fix

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,7 +1,7 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-SELECT customers.company_name, CONCAT(employees.first_name, ' ', employees.last_name) AS employee
+SELECT customers.company_name AS customer, CONCAT(employees.first_name, ' ', employees.last_name) AS employee
 FROM orders
 JOIN customers USING(customer_id)
 JOIN employees USING(employee_id)
@@ -18,7 +18,7 @@ JOIN categories USING (category_id)
 WHERE discontinued = 0
     AND units_in_stock < 25
     AND category_name IN ('Condiments', 'Dairy Products')
-ORDER BY units_in_stock
+ORDER BY 2
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 SELECT company_name FROM customers
